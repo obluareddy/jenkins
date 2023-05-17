bff8e88af12881ee6d44c1e1c94146304a6148e0
root <root@ip-172-31-39-102.ap-south-1.compute.internal>
2023-05-17 04:05:52 +0000

    first commit

---
+++ jenkins/src/main/java/com/maroix/App.java
@@ -0,0 +1,13 @@
+package com.maroix;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
